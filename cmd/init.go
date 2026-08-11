e22ccf74f5cbc52ebc9f172b7179afb85273c4b0
Mohamed Elmorsy <mohamedmahomed403@gmail.com>
2026-08-11 22:43:12 +0300

    Init command flags structure

--- cmd/init.go
+++ cmd/init.go
@@ -1,40 +1,76 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
 
+	"github.com/mohamed8eo/dockdb/internal/database"
 	"github.com/spf13/cobra"
+)
+
+var (
+	name     string
+	dbType   string
+	port     string
+	password string
+	detached bool
 )
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Create and start a database container",
+	Long: `Initialize a database by creating and starting a Docker container.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Supported databases:
+  postgres, pgsql, pg
+  mysql
+
+The database runs in detached mode by default only when the --detached flag
+is provided.
+
+Examples:
+  # Create a PostgreSQL container
+  mycli init --db postgres
+
+  # Create a PostgreSQL container using an alias
+  mycli init --db pg
+
+  # Create a MySQL container
+  mycli init --db mysql
+
+  # Customize the container
+  mycli init --db postgres --name my-db --port 5432 --password secret
+
+  # Run the container in the background
+  mycli init --db postgres --detached`,
 	Run: func(cmd *cobra.Command, args []string) {
+		db, err := database.ParseType(dbType)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
 		fmt.Println("init called")
+		fmt.Println(name)
+		fmt.Println(port)
+		fmt.Println(password)
+		fmt.Println(detached)
+		fmt.Println(db)
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(initCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//Flags
+	initCmd.Flags().StringVarP(&name, "name", "n", "postgresql", "db name")
+	initCmd.Flags().StringVarP(&port, "port", "p", "5432", "db port")
+	initCmd.Flags().StringVarP(&password, "password", "e", "postgresql", "db password")
+	initCmd.Flags().StringVarP(&dbType, "db", "t", "postgres", "database type (postgres or mysql)")
+	//run on backgroun
+	initCmd.Flags().BoolVarP(&detached, "detached", "d", false, "db detached")
 }
